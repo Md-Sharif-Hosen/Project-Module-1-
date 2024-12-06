1712f19a5cdf9c94222571126134bf4163828d90
sharif-hosen <mdsharifkhan762@gmail.com>
2024-12-06 22:44:54 +0600

    Simple Electricity bill calculate

--- Simple_Electricity_Bill_Calculate.php
+++ Simple_Electricity_Bill_Calculate.php
@@ -1,11 +1,11 @@
 <?php
 /*
 ! Simple Electricity Bill Calculator
-* Concepts Used: Variables, If-Else
-* Description:
-* Use a variable for units consumed.
-* Define slabs for billing using if-else (e.g., 1–100 units = $5, 101–200 units = $10,  201 to above units = $15).
-* Calculate and display the total bill based on the entered units.
+  * Concepts Used: Variables, If-Else
+  * Description:
+  * Use a variable for units consumed.
+  * Define slabs for billing using if-else (e.g., 1–100 units = $5, 101–200 units = $10,  201 to above units = $15).
+  * Calculate and display the total bill based on the entered units.
 */
 
 echo "Enter Units you Consumed: ";
